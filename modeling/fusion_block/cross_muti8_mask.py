debe7ab2823ef1343e4f461e51e81872829bb3bb
董秋彤 <118411625+ddddqt@users.noreply.github.com>
2025-03-16 10:49:37 +0800

    Update cross_muti8_mask.py

--- modeling/fusion_block/cross_muti8_mask.py
+++ modeling/fusion_block/cross_muti8_mask.py
@@ -1,6 +1,6 @@
 #Other code will be uploaded after the paper is officially accepted and published.
 
-def fusion_use_crossmulti8_mask5(x,y,keep_rate,enhance_weights,csnum_heads):
+def AFF(x,y,keep_rate,enhance_weights,csnum_heads):
     attn = EVAD_fusion(dim=1024, num_heads=8, csnum_heads=csnum_heads,qkv_bias=False, qk_scale=1, attn_drop=0., proj_drop=0.,
                      norm_layer=nn.LayerNorm, mlp_ratio=4., act_layer=nn.GELU, drop=0.,drop_path=0.,
                      init_values=0, enhance_weights=enhance_weights, keep_rate=keep_rate)
