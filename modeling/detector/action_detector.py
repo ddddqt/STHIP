d610e097bd50295afcb9ab7bb510b825a4ba3409
董秋彤 <118411625+ddddqt@users.noreply.github.com>
2025-03-16 10:51:46 +0800

    Update action_detector.py

--- modeling/detector/action_detector.py
+++ modeling/detector/action_detector.py
@@ -4,10 +4,6 @@ import numpy as np
 
 from hit.modeling.backbone import build_backbone
 from hit.modeling.roi_heads.roi_heads_3d import build_3d_roi_heads
-# print("4")
-# from hit.parsing.ParsingTransform import ParsingTransform
-# from hit.modeling.parsing.build_parsing import build_parsing
-# from hit.modeling.parsing.build_parsing_fusion import build_parsing_fusion
 
 class ActionDetector(nn.Module):
     def __init__(self, cfg):
@@ -15,9 +11,6 @@ class ActionDetector(nn.Module):
         self.cfg = cfg
         self.backbone = build_backbone(cfg)
         self.roi_heads = build_3d_roi_heads(cfg, self.backbone.dim_out)
-        # self.parsing = build_parsing(cfg)
-        self.parsing_use = cfg.DATASETS.PARSING_USE
-        # self.parsing_slowfast = build_parsing_fusion(cfg)
 
     def forward(self, slow_video, fast_video, boxes, objects=None, keypoints=None, extras={}, part_forward=-1):
 
@@ -26,28 +19,7 @@ class ActionDetector(nn.Module):
         else:
             slow_features, fast_features = self.backbone(slow_video, fast_video)
 
-        parsing_features = None
-        if part_forward == 1:
-            parsing_features = None
-        else:
-            if self.parsing_use == True:
-                # parsing_slow = ParsingTransform(slow_video, aug=self.cfg.DATASETS.PARSING_AUG)
-                # parsing_fast = ParsingTransform(fast_video, aug=self.cfg.DATASETS.PARSING_AUG)
-                # parsing = torch.stack((parsing_slow, parsing_fast), dim=1)
-                # print("parsing_1",parsin)
-
-                if self.cfg.PARSING.STRAGE == "fusion_in_slowfast_out":
-                    # parsing_features = self.parsing(parsing)
-                    slow_features,fast_features = self.parsing_slowfast(slow_features, fast_features, parsing_features)
-                # elif self.cfg.PARSING.STRAGE == "parsingconv_fusion_in_slowfast_out":
-                #     parsing_features = self.parsing(parsing)
-                #     print("parsing",parsing.shape)
-                #     slow_features,fast_features = self.parsing_slowfast(slow_features, fast_features, parsing_features)
-                # elif self.cfg.PARSING.STRAGE == "noparsing_fusion_in_slowfast_out":
-                #     # parsing = torch.stack((parsing_slow,parsing_fast))
-                #     slow_features,fast_features = self.parsing_slowfast(slow_features, fast_features, parsing)
-
-        result, detector_losses, loss_weight, detector_metrics = self.roi_heads(slow_features, fast_features, boxes, objects, keypoints, extras, part_forward, parsing_features)
+        result, detector_losses, loss_weight, detector_metrics = self.roi_heads(slow_features, fast_features, boxes, objects, keypoints, extras, part_forward)
 
         if self.training:
             return detector_losses, loss_weight, detector_metrics, result
